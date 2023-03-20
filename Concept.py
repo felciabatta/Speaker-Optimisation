203a1a638fdf1324cf2caddb1ae96e46f3901786
FelixxDP <felix.dp.2020@bristol.ac.uk>
2023-03-20 13:48:21 +0000

    experimenting

--- Concept.py
+++ Concept.py
@@ -1,14 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  7 12:19:03 2023
-
-@author: henryhutchings
-"""
-
 import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as spla
+import matplotlib.pyplot as plt
 
 # Set up the grid
 N = 50  # number of grid points in each direction
@@ -20,7 +13,10 @@ X, Y = np.meshgrid(x, y)
 
 # Define the source function
 f = np.zeros((N, N))
-f[4, 4] = 1  # point source in corner
+f[1:15, 1:15] = 1  # point source in corner
+f[1:15, 1:15] = 1  # point source in corner
+f[1:15, 35:49] = 1  # point source in other corner
+# f[20:49, 23:26] = 1
 
 # Set up the finite difference matrix
 A = sp.diags([-1, 2, -1], [-1, 0, 1], shape=(N-2, N-2))
@@ -40,7 +36,6 @@ b = f[1:-1, 1:-1].flatten()  # rhs of the system
 u[1:-1, 1:-1] = spla.spsolve(Lap, b).reshape(N-2, N-2)
 
 # Plot the solution
-import matplotlib.pyplot as plt
 plt.contourf(X, Y, u)
 plt.colorbar()
 plt.show()
