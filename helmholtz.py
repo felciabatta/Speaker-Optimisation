822e0d97f6d1b40ac1a3aab1770563bdb5d3de4c
FelixxDP <felix.dp.2020@bristol.ac.uk>
2023-04-24 12:13:53 +0100

    modularised fenicsx code

--- helmholtz.py
+++ helmholtz.py
@@ -15,66 +15,75 @@ import ufl
 import pyvista
 import dolfinx.plot
 
-meshdir = "geomeshes"
-geo_filename = get_path("geomeshes/square room/square room.geo")
-# geo_filename = get_path("geomeshes/experimental/enclosed speaker.geo")
+# CONSTANTS
+C = 343  # speed of sound
+RHO = 1.293  # density of air
+MESHDIR = "geomeshes/"
 
-gmsh.initialize()
-gmsh.open(geo_filename)
-gmsh.model.occ.synchronize()
-gdim = 2
-gmsh.option.setNumber("Mesh.MeshSizeMax", 0.1)
-gmsh.model.mesh.generate(gdim)
+def geo2mesh(geofile="square room/square room", max_size=0.1):
 
-# convert to dolfinx mesh
-gmsh_model_rank = 0
-mesh_comm = MPI.COMM_WORLD
-omega, cell_markers, facet_markers = gmshio.model_to_mesh(
-    gmsh.model, mesh_comm, gmsh_model_rank, gdim=gdim)
+    geo_path = get_path(MESHDIR+geofile+".geo")
+    gdim = 2
 
-gmsh.finalize()
+    gmsh.initialize()
+    gmsh.open(geo_path)
+    gmsh.model.occ.synchronize()
+    gmsh.option.setNumber("Mesh.MeshSizeMax", max_size)
+    gmsh.model.mesh.generate(gdim)
 
-# define function space (field)
-P = FunctionSpace(omega, ("Lagrange", 1))
-P_source = FunctionSpace(omega, ("DG", 0))
+    # convert to dolfinx mesh
+    gmsh_model_rank = 0
+    mesh_comm = MPI.COMM_WORLD
+    omega_mesh, cell_markers, facet_markers = gmshio.model_to_mesh(
+        gmsh.model, mesh_comm, gmsh_model_rank, gdim=gdim)
 
-# define unknown and test variables
-p = ufl.TrialFunction(P)
-q = ufl.TestFunction(P)
+    gmsh.finalize()
+    
+    return omega_mesh, cell_markers, facet_markers
 
-# constants
-c = 343  # speed of sound
-rho = 1.293  # density of air
-f = 20  # frequency
-w = 2*pi*f  # radial frequency
-k = w/c  # wave number
-Q_strength = 1 # monopole source strength
-Q = Function(P_source)
-source_cells = cell_markers.find(2)
-Q.x.array[source_cells] = Q_strength
+def solve_helmholtz(omega_mesh, cell_markers, facet_markers, f=20):
+    # define function space (field)
+    P = FunctionSpace(omega_mesh, ("Lagrange", 1))
+    P_source = FunctionSpace(omega_mesh, ("DG", 0))
 
-# define weak form
-a = ufl.inner(ufl.grad(p), ufl.grad(q))*ufl.dx - k**2 * ufl.inner(p, q) * ufl.dx
-L = rho * ufl.inner(Q, q) * ufl.dx
+    # define unknown and test variables
+    p = ufl.TrialFunction(P)
+    q = ufl.TestFunction(P)
 
-# solve
-problem = LinearProblem(a, L, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
-uh = problem.solve()
+    # constants
+    w = 2*pi*f  # radial frequency
+    k = w/C  # wave number
+    Q_strength = 1 # monopole source strength
+    Q = Function(P_source)
+    source_cells = cell_markers.find(2)
+    Q.x.array[source_cells] = Q_strength
 
-# SET UP SOLUTION PLOT DATA
-# make grid
-u_topology, u_cell_types, u_geometry = dolfinx.plot.create_vtk_mesh(P)
-u_grid = pyvista.UnstructuredGrid(u_topology, u_cell_types, u_geometry)
-# add point data
-u_grid.point_data["u"] = uh.x.array.real
-u_grid.set_active_scalars("u")
-warped = u_grid.warp_by_scalar()
+    # define weak form
+    a = ufl.inner(ufl.grad(p), ufl.grad(q))*ufl.dx - k**2 * ufl.inner(p, q) * ufl.dx
+    L = RHO * ufl.inner(Q, q) * ufl.dx
 
-# PLOT SOLUTION
-# initialise plotter
-u_plotter = pyvista.Plotter()
-# add data
-u_plotter.add_mesh(u_grid, show_edges=0, cmap='seismic')
-# view
-u_plotter.view_xy()
-u_plotter.show()
+    # solve
+    problem = LinearProblem(a, L, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+    return problem.solve()
+
+def plot_solution(uh, warped=False):
+    P = uh.ufl_function_space()
+    
+    # SET UP SOLUTION PLOT DATA
+    # make grid
+    u_topology, u_cell_types, u_geometry = dolfinx.plot.create_vtk_mesh(P)
+    u_grid = pyvista.UnstructuredGrid(u_topology, u_cell_types, u_geometry)
+    # add point data
+    u_grid.point_data["u"] = uh.x.array.real
+    u_grid.set_active_scalars("u")
+    if warped:
+        u_grid = u_grid.warp_by_scalar()
+
+    # PLOT SOLUTION
+    # initialise plotter
+    u_plotter = pyvista.Plotter()
+    # add data
+    u_plotter.add_mesh(u_grid, show_edges=0, cmap='seismic')
+    # view
+    u_plotter.view_xy()
+    u_plotter.show()
