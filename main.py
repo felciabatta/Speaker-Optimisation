f4a3004b3a1f2d4d61eceb393c6d6e65e1e8b52e
FelixxDP <felix.dp.2020@bristol.ac.uk>
2023-04-25 17:01:23 +0100

    now use python mesh, rather than .geo

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 """Run Helmholtz simulations."""
 
-from helmholtz import geo2mesh, solve_helmholtz, plot_solution
+from helmholtz import geo2mesh, helmholtz_problem, plot_solution
+from geometry import basic_room
+from numpy import pi
 
-def one_frequency_example(geofile="square room/square room", f=20, max_size=0.1,
-                          warp=0):
-    mesh, cells, facets = geo2mesh(geofile, max_size)
-    ph = solve_helmholtz(mesh, cells, facets, f)
-    plot_solution(ph, warp=warp)
-    return ph
+def one_frequency_example(geofunc=basic_room, pos=(5,5), angle=0*pi, f=20, 
+                          max_size=0.1, warp=0):
+    (_, source), mesh, cells, facets = geofunc(pos, angle, max_size=max_size, 
+                                             to_dolfin=True)
+    ph = helmholtz_problem(mesh, cells, facets, source, f).solve()
+    p_grid = plot_solution(ph, warp=warp)
+    return ph, p_grid
